2f143cf95476ab497604281262ce2eaa81b12327
Lulika57 <kharchenko.anastasya2013@gmail.com>
2026-01-13 23:01:26 -0600

    1768. Merge Strings Alternately

---
+++ 1768-alt-merge-strings.kt
@@ -0,0 +1,15 @@
+
+
+class Solution {
+    fun mergeAlternately(word1: String, word2: String): String {
+        val builder = StringBuilder(word1.length + word2.length)
+        val maxLength = maxOf(word1.length, word2.length)
+
+        for(i in 0 until maxLength) {
+            if(i < word1.length) builder.append(word1[i]) 
+            if(i < word2.length) builder.append(word2[i]) 
+        }
+
+        return builder.toString()
+    }
+}
