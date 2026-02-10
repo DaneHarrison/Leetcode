f1d5aea6a27dd6f0f90e56e89b68453499bf9eee
Dane <info.danewanke@gmail.com>
2026-02-10 13:48:31 -0600

    longest zig zag

---
+++ medium/1372-count-zig-zag.kt
@@ -0,0 +1,20 @@
+fun longestZigZag(root: TreeNode?): Int {
+    fun dfs(root: TreeNode?, goRightNext: Boolean, count: Int): Int {
+        if(root == null) {
+            return count - 1
+        }
+
+        val nextOnContinue = if(goRightNext) root?.right else root?.left
+        val nextOnReset = if(goRightNext) root?.left else root?.right
+
+        val continueCount = dfs(nextOnContinue, !goRightNext, count + 1)
+        val resetCount = dfs(nextOnReset, goRightNext, 1)
+
+        return maxOf(continueCount, resetCount)
+    }
+
+    return maxOf(
+        dfs(root?.left, true, 1),
+        dfs(root?.right, false, 1)
+    )
+}
